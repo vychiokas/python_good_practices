84395d3780ca09c25bee7652e002d19833db8baf
Vytautas Sluckas <vytautas.sluckas@exacaster.com>
2023-05-26 15:20:07 +0300

    untidy example extended

--- code_formatting/examples/not_tidy_example.py
+++ code_formatting/examples/not_tidy_example.py
@@ -1,16 +1,18 @@
 from typing import Dict
+
+
 def my_function(parameter1: str, parameter2: int, parameter3: Dict[str, str]) -> None:
     print(parameter1, parameter2, parameter3)
 
 
-my_dictionary = {"name":"value","another name":"another value"}
+my_dictionary = {"name": "value", "another name": "another value"}
 
 
 
-def is_unique(
+def is_unique( 
                s
                ):
-    s = list(s
+    s = list(s 
                 )
     s.sort()
  
@@ -20,9 +22,21 @@ def is_unique(
             return 0
     else:
         return 1
- 
- 
+#fmt: off
+def my_function2(df, operator_clasificator, phone_number_col_name, temp_country_code_col_name, new_operator_col_name, F) -> None:
+    df = (
+        df.alias("left")
+        .join(operator_clasificator.alias("right"),
+            F.expr(f"""substr(left.{phone_number_col_name}, 
+                   length(left.{temp_country_code_col_name}) +1) == right.msisdn"""),
+            how="left_outer")
+        .select("left.*", F.col("right.operator_name").alias(new_operator_col_name))
+    )
+#fmt: on
 if __name__ == "__main__":
     print(
           is_unique(input())
          )
+
+
+
